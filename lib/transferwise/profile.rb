d26aafe597f69613bf4f44ff9ef1d3dccfbf422e
Farhan <terfender@gmail.com>
2021-10-21 18:20:16 -0400

    Added fund method to Profile class

--- lib/transferwise/profile.rb
+++ lib/transferwise/profile.rb
@@ -1,4 +1,11 @@
 module Transferwise
   class Profile < APIResource
+    def self.fund(profile_id, transfer_id, headers)
+      url = "#{resource_url(profile_id, api_version: 'v3')}/transfers/#{transfer_id}/payments"
+
+      params = { 'type' => 'BALANCE' }
+      response = Request.request(:post, url, params, headers)
+      convert_to_transferwise_object(response)
+    end
   end
 end
