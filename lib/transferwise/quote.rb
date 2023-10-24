dfe3cddea52b1f3a48d2aa02aae5b378df3b6e6a
Simon Rand <simonrand@gmail.com>
2023-10-24 16:50:37 +0100

    Update Quote#create endpoint

--- lib/transferwise/quote.rb
+++ lib/transferwise/quote.rb
@@ -1,4 +1,8 @@
 module Transferwise
   class Quote < APIResource
+    def self.create(params = {}, opts = {})
+      response = Transferwise::Request.request(:post, collection_url(nil, api_version: 'v3'), params, opts)
+      convert_to_transferwise_object(response)
+    end
   end
 end
