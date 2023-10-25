cf425f64043ec57cf37bcf575087c6b5b9c8cf88
Simon Rand <simonrand@gmail.com>
2023-10-25 13:15:40 +0100

    Move Quote apis to v3

--- lib/transferwise/quote.rb
+++ lib/transferwise/quote.rb
@@ -1,7 +1,14 @@
 module Transferwise
   class Quote < APIResource
-    def self.create(params = {}, opts = {})
-      response = Transferwise::Request.request(:post, collection_url(nil, api_version: 'v3'), params, opts)
+    API_VERSION = 'v3'
+
+    def self.collection_url(resource_id = nil, api_version: API_VERSION)
+      super(resource_id, api_version:)
+    end
+
+    def self.create_authenticated(profile_id, params = {}, opts = {})
+      url = "/#{API_VERSION}/profiles/#{profile_id}/quotes"
+      response = Transferwise::Request.request(:post, url, params, opts)
       convert_to_transferwise_object(response)
     end
   end
